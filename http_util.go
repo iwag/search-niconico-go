e9014ac1b219b3aa203e7095535c4d19b913cd44
iwag <iwagame@gmail.com>
2016-12-14 23:08:43 +0900

    format

--- http_util.go
+++ http_util.go
@@ -6,8 +6,8 @@ import (
 	"io/ioutil"
 	"net/http"
 	"net/url"
-  "log"
-  "os"
+	"log"
+	"os"
 )
 
 const SEARCH_API = "http://api.search.nicovideo.jp/"
@@ -34,7 +34,7 @@ func parseJson(body io.ReadCloser, intf *interface{}, debug bool) error {
 }
 
 func post(path string, values url.Values, intf interface{}, debug bool) error {
-  resp, err := HTTPClient.PostForm(SEARCH_API + path, values)
+	resp, err := HTTPClient.PostForm(SEARCH_API + path, values)
 	if err != nil {
 		return err
 	}
--- search.go
+++ search.go
@@ -4,27 +4,27 @@ import (
 	"errors"
 	"net/url"
 	"strconv"
-  "fmt"
+	"fmt"
 )
 
 const (
-	DEFAULT_SEARCH_SORT      = "startTime"
-	DEFAULT_SEARCH_TARGETS  = "tags,title"
+	DEFAULT_SEARCH_SORT = "startTime"
+	DEFAULT_SEARCH_TARGETS = "tags,title"
 	DEFAULT_SEARCH_FIELDS = "contentId,title,tags,startTime"
-	DEFAULT_SEARCH_LIMIT      = 25
+	DEFAULT_SEARCH_LIMIT = 25
 )
 
 type SearchParameters struct {
-  Targets   string
-  Fields    string
-	Sort      string
-	Limit     int
+	Targets string
+	Fields  string
+	Sort    string
+	Limit   int
 }
 
 type MetaResponse struct {
 	Code       int    `json:"status"`
-  TotalCount int    `json:"totalCount"`
-  Id         string `json:"id"`
+	TotalCount int    `json:"totalCount"`
+	Id         string `json:"id"`
 }
 
 type SearchResponse struct {
@@ -34,8 +34,8 @@ type SearchResponse struct {
 func CreateSearchParameters() SearchParameters {
 	return SearchParameters{
 		Sort:     DEFAULT_SEARCH_SORT,
-    Targets:  DEFAULT_SEARCH_TARGETS,
-    Fields:   DEFAULT_SEARCH_FIELDS,
+		Targets:  DEFAULT_SEARCH_TARGETS,
+		Fields:   DEFAULT_SEARCH_FIELDS,
 		Limit:    DEFAULT_SEARCH_LIMIT,
 	}
 }
@@ -62,26 +62,27 @@ func (client *Client) _search(path, query string, params SearchParameters) (resp
 	if err := post(path, queries, response, client.debug); err != nil {
 		return nil, err
 	}
-	if response.MetaResponse.Code == 200 {
+	if response.MetaResponse.Code != 200 {
+		logger.Printf("%v\n", response.MetaResponse)
 		return nil, errors.New("something wrong")
 	}
 	return response, nil
 }
 
 func (client *Client) Search(path, query string, params SearchParameters) (*MetaResponse, error) {
-	if response, err := api._search(path, query, params); err != nil {
+	if response, err := client._search(path, query, params); err != nil {
 		return nil, err
 	} else {
-	  return &response.MetaResponse, nil
-  }
+		return &response.MetaResponse, nil
+	}
 }
 
 func main() {
-    client := New()
-    res, err := client.Search("v2/video/contents/search", "test", CreateSearchParameters())
-    if err != nil {
-        fmt.Printf("%s\n", err)
-        return
-    }
-    fmt.Printf("response: %v\n", res)
+	client := New()
+	res, err := client.Search("v2/video/contents/search", "test", CreateSearchParameters())
+	if err != nil {
+		fmt.Printf("%s\n", err)
+		return
+	}
+	fmt.Printf("response: %v\n", res)
 }
